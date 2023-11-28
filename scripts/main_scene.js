251caf27823d30aa8646c825c7ec605befd4a389
renka <r.oomori.sys22@morijyobi.ac.jp>
2023-11-28 14:44:37 +0900

    演習1-3

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -12,10 +12,22 @@ class MainScene extends Phaser.Scene {
 
     create() {
         this.add.image(400, 225, 'back');
-        this.add.image(400, 300, 'taro');
+        this.player = this.add.image(400, 300, 'taro');
+        this.player_direction = 1;
     }
 
     update() {
+        if (this.player.x >= D_WIDTH - 50) this.player_direction = -1;
+        if (this.player.y >= D_HEIGHT - 50) this.player_direction = -1;
+        if (this.player.x <= 50) this.player_direction = 1;
+        if (this.player.y <= 50) this.player_direction = 1;
+        if (this.player_direction == 1) {
+            this.player.x += 5;
+            this.player.y += 5;
+        } else {
 
+            this.player.x -= 5;
+            this.player.y -= 5;
+        }
     }
 }

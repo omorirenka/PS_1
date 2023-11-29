1984c43cb961524646fd2433a71209bdc49ad667
renka <r.oomori.sys22@morijyobi.ac.jp>
2023-11-29 14:30:37 +0900

    演習1-4

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -18,33 +18,17 @@ class MainScene extends Phaser.Scene {
     }
 
     update(time, delta)  {
-        if (this.player.x >= D_WIDTH - 200) this.player_direction = -1;
-        if (this.player.x <= 250) this.player_direction = 1;
-
-        if (this.player_direction == 1) {
-            this.player.setVelocity(200, -200);
-        } else {
-            this.player.setVelocity(-200, 200);
-         }
-
-        // 回転角度を更新
-        // this.player.angle += 5;
-        // 回転角度を設定
-        // this.player.setAngle( this.player.angle );
-        // if (this.player.x >= D_WIDTH - 50) this.player_direction = -1;
-        // if (this.player.y >= D_HEIGHT - 50) this.player_direction = 1;
-        // if (this.player.x <= 50) this.player_direction = 1;
-        // if (this.player.y <= 50) this.player_direction = -1;
+        // if (this.player.x >= D_WIDTH - 200) this.player_direction = -1;
+        // if (this.player.x <= 250) this.player_direction = 1;
         // if (this.player_direction == 1) {
-        //     this.player.angle += 5;
-        //     this.player.setAngle( this.player.angle );
-        //     this.player.x += 5;
-        //     this.player.y -= 5;
+        //     this.player.setVelocity(200, -200);
         // } else {
-        //     this.player.angle += 5;
-        //     this.player.setAngle( this.player.angle );
-        //     this.player.x = 400;
-        //     this.player.y = 250;
+        //     this.player.setVelocity(-200, 200);
         // }
+
+        this.player.setVelocity(200, -200);
+        this.player.angle += 5;
+        this.player.setAngle( this.player.angle );
+        
     }
 }
